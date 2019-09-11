54d68421af67af1392765afa251e3392ec0fc332
nonpro98 <55211709+nonpro98@users.noreply.github.com>
2019-09-11 22:21:13 +0200

    Add files via upload

---
+++ message_encrypter_1.0.py
@@ -0,0 +1,117 @@
+
+def encripter():
+    useroutput = (" ")
+    number = int (input("quante lettere ci sono?"))
+    print ("inserire la parola lettera per lettera")
+    for i in range (number + 1):
+        userinput = input ("")
+        useroutputbackup = useroutput
+        if userinput == ("a"):
+            useroutput = (useroutputbackup + "d")
+            useroutputbackup = useroutput
+            print (useroutputbackup)
+        elif userinput == ("b"):
+            useroutput = (useroutputbackup + "e")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("c"):
+            useroutput = (useroutputbackup + "f")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("d"):
+            useroutput = (useroutputbackup + "g")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("e"):
+            useroutput = (useroutputbackup + "h")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("f"):
+            useroutput = (useroutputbackup + "i")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("g"):
+            useroutput = (useroutputbackup + "j")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("h"):
+            useroutput = (useroutputbackup + "k")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("i"):
+            useroutput = (useroutputbackup + "l")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("j"):
+            useroutput = (useroutputbackup + "m")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("k"):
+            useroutput = (useroutputbackup + "n")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("l"):
+            useroutput = (useroutputbackup + "o")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("m"):
+            useroutput = (useroutputbackup + "p")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("n"):
+            useroutput = (useroutputbackup + "q")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("o"):
+            useroutput = (useroutputbackup + "r")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("p"):
+            useroutput = (useroutputbackup + "s")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("q"):
+            useroutput = (useroutputbackup + "t")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("r"):
+            useroutput = (useroutputbackup + "u")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("s"):
+            useroutput = (useroutputbackup + "v")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("t"):
+            useroutput = (useroutputbackup + "w")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("u"):
+            useroutput = (useroutputbackup + "x")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("v"):
+            useroutput = (useroutputbackup + "y")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("w"):
+            useroutput = (useroutputbackup + "z")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("x"):
+            useroutput = (useroutputbackup + "a")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("y"):
+            useroutput = (useroutputbackup + "b")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+        elif userinput == ("z"):
+            useroutput = (useroutputbackup + "c")
+            useroutputbackup = useroutput
+            print(useroutputbackup)
+
+
+
+
+encripter()
